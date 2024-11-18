7eab5168806c8eb89166da0ab33bd192aa2a8e83
beomju <kategory333@gmail.com>
2024-11-18 09:36:11 +0900

    pull 전 commit

--- bank_pjt/bank_pjt/settings.py
+++ bank_pjt/bank_pjt/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:8000/',
     "http://127.0.0.1:9000",
     "http://localhost:8080",
 )
