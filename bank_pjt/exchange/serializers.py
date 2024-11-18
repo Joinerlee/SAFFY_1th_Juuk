22c73fe08e0c26301726511135ea1001735f71cc
beomju <kategory333@gmail.com>
2024-11-18 14:58:34 +0900

    a

--- bank_pjt/exchange/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Exchange
-
-
-class ExchangeSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Exchange
-        fields = '__all__'
--- bank_pjt/exchange/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name='exchange'
-urlpatterns = [
-    path('', views.exchanges, name = 'exchanges'),
-    path('update/', views.update, name='update')
-]
