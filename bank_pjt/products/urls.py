c0b1873f663a6c167faa50dd80c4623bf056152f
beomju <kategory333@gmail.com>
2024-11-18 14:40:26 +0900

    회원가입,회원탈퇴, 예적금

--- bank_pjt/products/urls.py
+++ bank_pjt/products/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     # 정기 예금 전체 조회( 정기 예금 상품의 기간 별 금리 포함)
     path('term_deposit/', views.term_deposit, name='term_deposit'),
     # 상세 정기 예금 상품 조회
-    path('term_deposit/detail/<fin_prdt_cd>/', views.term_deposit_detail, name='term_deposit_detail'),
-    
+    path('term_deposit/detail/<fin_prdt_cd>/', views.term_deposit_detail, name='term_deposit_detail'), 
     # 적금 가져오기
     path('savings/update/', views.savings_update, name='savings_update'),
     # 적금 전체 조회( 정기 예금 상품의 기간 별 금리 포함)
